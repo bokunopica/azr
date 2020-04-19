5b0909bc99105a97594741f9d2a7a96f80c506f3
bokunopica <comehome@foxmail.com>
2020-04-19 19:39:38 +0800

    test commit 2

--- AZRWebApi/user_api.py
+++ AZRWebApi/user_api.py
@@ -4,3 +4,8 @@ from flask_restful import Resource
 class user_api_resource(Resource):
     def get(self):
         return {"msg":"ok"}
+
+
+class user_login_register(Resource):
+    def get(self):
+        return {"msg":"ok"}
